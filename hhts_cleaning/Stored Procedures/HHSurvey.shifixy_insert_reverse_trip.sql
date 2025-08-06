1920f5aa5a8811386227e89eef241ce6d27bd837
jensenmj <mjensen@psrc.org>
2025-08-06 13:37:34 -0700

    Fix datatype and rename parameter.

--- hhts_cleaning/Stored Procedures/HHSurvey.shifixy_insert_reverse_trip.sql
+++ hhts_cleaning/Stored Procedures/HHSurvey.shifixy_insert_reverse_trip.sql
@@ -3,13 +3,13 @@ GO
 SET ANSI_NULLS ON
 GO
 CREATE PROCEDURE [HHSurvey].[shifixy_insert_reverse_trip]
-    @recid int, 
+    @target_recid int, 
     @startdatetime nvarchar(19)
 AS
 BEGIN
-    DECLARE @target_person_id int;
-    SET @target_person_id = (SELECT person_id FROM HHSurvey.trip WHERE recid = @recid);
-    EXECUTE HHSurvey.insert_reverse_trip @recid, @startdatetime;
+    DECLARE @target_person_id decimal;
+    SET @target_person_id = (SELECT person_id FROM HHSurvey.trip WHERE recid = @target_recid);
+    EXECUTE HHSurvey.insert_reverse_trip @target_recid, @startdatetime;
     EXECUTE HHSurvey.recalculate_after_edit @target_person_id;
     EXECUTE HHSurvey.generate_error_flags @target_person_id;
 END
