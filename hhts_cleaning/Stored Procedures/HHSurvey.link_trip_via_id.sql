96d5a63842636c5ab9e6867279712af9956f821c
christopeak <cpeak@psrc.org>
2025-08-18 14:49:29 -0700

    convert HHSurvey.trip_ingredient to #trip_ingredient

--- hhts_cleaning/Stored Procedures/HHSurvey.link_trip_via_id.sql
+++ hhts_cleaning/Stored Procedures/HHSurvey.link_trip_via_id.sql
@@ -15,28 +15,28 @@ AS BEGIN
 	END
 	
 	BEGIN TRY
-		DROP TABLE IF EXISTS HHSurvey.trip_ingredient;
+		DROP TABLE IF EXISTS #trip_ingredient;
 
 		-- insert recid's into #recid_list
 		SELECT CAST(Elmer.dbo.TRIM(value) AS int) AS recid INTO #recid_list 
 			FROM STRING_SPLIT(@recid_list, ',')
 			WHERE RTRIM(value) <> '';
 		
-		-- find the earliest trip segment and insert it into [trip_ingredient] table
+		-- find the earliest trip segment and insert it into [#trip_ingredient] table
 		WITH cte AS (SELECT TOP 1 tripnum AS trip_link FROM HHSurvey.trip AS t JOIN #recid_list AS rid ON rid.recid = t.recid ORDER BY t.depart_time_timestamp)
-		SELECT t.*, cte.trip_link INTO HHSurvey.trip_ingredient
+		SELECT t.*, cte.trip_link INTO #trip_ingredient
 			FROM HHSurvey.trip AS t JOIN cte ON 1 = 1
 			WHERE EXISTS (SELECT 1 FROM #recid_list AS rid WHERE rid.recid = t.recid);
 		
-		-- Set @person_id to the person in trip_ingredient
+		-- Set @person_id to the person in #trip_ingredient
 		DECLARE @person_id decimal(19,0) = NULL
-		SET @person_id = (SELECT person_id FROM HHSurvey.trip_ingredient GROUP BY person_id);
+		SET @person_id = (SELECT person_id FROM #trip_ingredient GROUP BY person_id);
 
 		EXECUTE HHSurvey.link_trips;
 		EXECUTE HHSurvey.tripnum_update @person_id;
 		EXECUTE HHSurvey.generate_error_flags @person_id;
 		DROP TABLE IF EXISTS #recid_list;
-		DROP TABLE IF EXISTS HHSurvey.trip_ingredient;
+		DROP TABLE IF EXISTS #trip_ingredient;
 		SET @person_id = NULL
 		SET @recid_list = NULL
 		
@@ -47,7 +47,7 @@ AS BEGIN
 	BEGIN CATCH
 		-- Cleanup on error
 		DROP TABLE IF EXISTS #recid_list;
-		DROP TABLE IF EXISTS HHSurvey.trip_ingredient;
+		DROP TABLE IF EXISTS #trip_ingredient;
 		
 		-- Transaction rollback
 		IF @started_transaction = 1 AND @@TRANCOUNT > 0 AND XACT_STATE() <> 0
