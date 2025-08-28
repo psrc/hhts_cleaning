1c221073cf0aea76398f225fe62f8f2554cfe6a6
jensenmj <mjensen@psrc.org>
2025-08-28 15:07:14 -0700

    Add tracking to prevent requerying API for the same record.

--- hhts_cleaning/Stored Procedures/HHSurvey.impute_purpose_from_location.sql
+++ hhts_cleaning/Stored Procedures/HHSurvey.impute_purpose_from_location.sql
@@ -2,7 +2,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-CREATE PROCEDURE [HHSurvey].[impute_purpose_from_location] @GoogleKey nvarchar
+CREATE PROCEDURE [HHSurvey].[impute_purpose_from_location] @GoogleKey nvarchar(100)
 AS BEGIN 
 
     BEGIN TRANSACTION;
@@ -10,25 +10,25 @@ AS BEGIN
     COMMIT TRANSACTION;
 
     BEGIN TRANSACTION;
-    SELECT t.recid, t.dest_lat, t.dest_lng, DATEDIFF(Minute, t.arrival_time_timestamp, nxt.depart_time_timestamp) AS dwell, CAST('' AS nvarchar(255)) AS loc_result, 0 AS new_dest_purpose
+    SELECT t.recid, t.dest_lat, t.dest_lng, DATEDIFF(Minute, t.arrival_time_timestamp, nxt.depart_time_timestamp) AS dwell, CAST('PENDING' AS nvarchar(255)) AS loc_result, 0 AS new_dest_purpose
     INTO dbo.api_purpose 
         FROM HHSurvey.Trip AS t LEFT JOIN HHSurvey.Trip AS nxt ON t.person_id = nxt.person_id AND t.tripnum +1 = nxt.tripnum JOIN HHSurvey.Person AS p ON t.person_id = p.person_id
         WHERE t.dest_purpose IN(SELECT flag_value FROM HHSurvey.NullFlags UNION SELECT 97) OR (t.dest_purpose=60 AND DATEDIFF(Minute, t.arrival_time_timestamp, nxt.depart_time_timestamp) > 60)
         AND t.dest_is_home<>1 AND t.dest_is_work<>1;
-    COMMIT TRANSACTION	;		
+    COMMIT TRANSACTION;		
 
     DECLARE @i int
-    SET @i = (SELECT count(*) FROM dbo.api_purpose WHERE loc_result ='')
+    SET @i = (SELECT count(*) FROM dbo.api_purpose WHERE loc_result = 'PENDING')
     WHILE @i > 0
     BEGIN 
         BEGIN TRANSACTION;
         UPDATE TOP (25) dbo.api_purpose  -- Batch to avoid API limits
         SET loc_result = Elmer.dbo.loc_recognize(dest_lng, dest_lat, @GoogleKey)
-        WHERE loc_result ='';
+        WHERE loc_result = 'PENDING';
 
         WAITFOR DELAY '00:00:02'; 
 
-        SET @i = (SELECT count(*) FROM dbo.api_purpose WHERE loc_result ='')
+        SET @i = (SELECT count(*) FROM dbo.api_purpose WHERE loc_result = 'PENDING')
         COMMIT TRANSACTION;
     END
 
