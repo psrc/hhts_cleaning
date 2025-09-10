584d1c141ee249d82d9f9f7a1236757a565af16a
christopeak <cpeak@psrc.org>
2025-09-10 16:38:07 -0700

    Added mohammad to hhts_cleaning permissions

--- hhts_cleaning/Security/Roles/data_editor.sql
+++ hhts_cleaning/Security/Roles/data_editor.sql
@@ -1,8 +1,10 @@
 CREATE ROLE [data_editor]
 AUTHORIZATION [dbo]
+EXEC sp_addrolemember N'data_editor', N'PSRC\MOshanreh'
+GRANT EXECUTE TO [data_editor]
+
 GO
 ALTER ROLE [data_editor] ADD MEMBER [PSRC\GGibson]
 GO
 ALTER ROLE [data_editor] ADD MEMBER [PSRC\MRichards]
 GO
-GRANT EXECUTE TO [data_editor]
