be782f8694dbc559f26e6a29c6086912376478c4
jensenmj <mjensen@psrc.org>
2025-09-19 15:59:25 -0700

    Reorder priority for inspection & correction.

--- hhts_cleaning/Views/HHSurvey.person_error_assignment.sql
+++ hhts_cleaning/Views/HHSurvey.person_error_assignment.sql
@@ -7,26 +7,27 @@ CREATE   VIEW [HHSurvey].[person_error_assignment] WITH SCHEMABINDING AS
 WITH FlagPriority AS (
     SELECT v.error_flag, v.priority
     FROM (VALUES
-        ('"change mode" purpose',1),
-        ('lone trip',2),
-        ('purpose missing',3),
-        ('mode_1 missing',4),
-        ('time overlap',5),
-        ('instantaneous',6),
-        ('excessive speed',7),
-        ('too slow',8),
-        ('same dest as prior',9),
-        ('purpose at odds w/ dest',10),
-        ('too long at dest?',11),
-        ('o purpose not equal to prior d purpose',12),
-        ('no activity time after',13),
-        ('missing next trip link',14),
-        ('ends day, not home',15),
-        ('starts, not from home',16),
-        ('initial trip purpose missing',17),
-        ('non-student + school trip',18),
-        ('non-worker + work trip',19),
-        ('underage_detailed driver',20)
+        ('underage driver',1),
+        ('non-worker + work trip',2),
+        ('non-student + school trip',3),
+        ('starts, not from home',4),
+        ('ends day, not home',5),
+        ('same dest as prior',6),
+        ('mode_1 missing',7),
+        ('lone trip',8),
+        ('missing next trip link',9),
+        ('initial trip purpose missing',10),
+        ('time overlap',11),
+        ('no activity time after',12),
+        ('o purpose not equal to prior d purpose',13),
+        ('time overlap',14),
+        ('"change mode" purpose',15),       
+        ('purpose missing',16),
+        ('instantaneous',17),
+        ('excessive speed',18),
+        ('too slow',19),
+        ('purpose at odds w/ dest',20),
+        ('too long at dest?',21)
     ) v(error_flag, priority)
 ), PersonFlags AS (
     SELECT t.person_id,
@@ -43,6 +44,5 @@ FROM (
     FROM PersonFlags
  ) x
 WHERE rn = 1;
-
 
 GO
