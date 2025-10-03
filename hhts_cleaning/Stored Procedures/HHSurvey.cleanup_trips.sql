c39f2f6d9ba541dfaab400842b5c7b338dee5171
jensenmj <mjensen@psrc.org>
2025-10-02 20:40:52 -0700

    Increase to same distance used in equating locations in fill_gaps

--- hhts_cleaning/Stored Procedures/HHSurvey.cleanup_trips.sql
+++ hhts_cleaning/Stored Procedures/HHSurvey.cleanup_trips.sql
@@ -3,7 +3,7 @@ GO
 SET ANSI_NULLS ON
 GO
 
-CREATE PROCEDURE [HHSurvey].[cleanup_trips]
+CREATE   PROCEDURE [HHSurvey].[cleanup_trips]
 AS BEGIN
 
 	-- Snap origin points to prior destination, when proximate
@@ -12,6 +12,6 @@ AS BEGIN
 		t.origin_lat=prev_t.dest_geog.Lat,
 		t.origin_lng=prev_t.dest_geog.Long
 		FROM HHSurvey.Trip AS t JOIN HHSurvey.Trip AS prev_t ON t.person_id=prev_t.person_id AND t.tripnum -1 = prev_t.tripnum
-		WHERE t.origin_geog.STEquals(prev_t.dest_geog)=0 AND t.origin_geog.STDistance(prev_t.dest_geog)<100;
+		WHERE t.origin_geog.STEquals(prev_t.dest_geog)=0 AND t.origin_geog.STDistance(prev_t.dest_geog)<150;
 END
 GO
